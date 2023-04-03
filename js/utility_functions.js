43d5949b51a8f583400022d43cb55fa98f744233
Miranda Cooper <mleighc@umich.edu>
2023-04-03 19:02:22 -0400

    testing out readmore button

---
+++ js/utility_functions.js
@@ -0,0 +1,114 @@
+// inspiration source: https://www.w3schools.com/howto/howto_js_read_more.asp
+// https://stackoverflow.com/questions/59360119/how-to-make-multiple-read-more-read-less-buttons-in-the-same-one-page
+// READ MORE BUTTONS
+function readMore(name) {
+    let dots = document.querySelector(`.card[data-name="${name}"] .dots`);
+    let moreText = document.querySelector(`.card[data-name="${name}"] .more`);
+    let btnText = document.querySelector(`.card[data-name="${name}"] .myBtn`);
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.textContent = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.textContent = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
+// FEM DICT BUTTONS
+// < !--Starting with the code from this source and then planning to make additional changes to adjust interaction for the terms-- >
+//     < !--source: https://www.w3schools.com/howto/howto_js_filter_elements.asp -->
+
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+// get all the buttons on the page
+const buttons = document.querySelectorAll('.btn');
+
+// loop through all the buttons and add a click event listener to each one
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // remove the active class from all buttons
+        buttons.forEach((b) => b.classList.remove('active'));
+
+        // add the active class to the clicked button
+        button.classList.add('active');
+    });
+});
+// var btnContainer = document.getElementById("myBtnContainer");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
+
+
+// ABOUT SLIDESHOW
+// sourced from w3schools: https://www.w3schools.com/howto/howto_js_quotes_slideshow.asp
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
